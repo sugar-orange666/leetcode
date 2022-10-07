36407fd601375b32d973af6f68a7dfa7a7eb6745
songwenjinger <3189064417@qq.com>
2022-10-07 21:08:33 +0800

    no226. 翻转二叉树

---
+++ src/com/demo/no02/No226.java
@@ -0,0 +1,18 @@
+package com.demo.no02;
+
+import com.common.TreeNode;
+
+public class No226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
