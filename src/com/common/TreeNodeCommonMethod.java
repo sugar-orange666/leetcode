b928066117e45a67bf1e603e327bbb7a69d7830d
songwenjinger <3189064417@qq.com>
2022-10-08 17:25:14 +0800

    no106 从中序与后序遍历序列构造二叉树

---
+++ src/com/common/TreeNodeCommonMethod.java
@@ -0,0 +1,38 @@
+package com.common;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TreeNodeCommonMethod {
+    //层序遍历
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.push(root);
+
+
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            List<Integer> cur = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode pop = deque.pop();
+                cur.add(pop.val);
+                if (pop.left != null) {
+                    deque.addLast(pop.left);
+                }
+                if (pop.right != null) {
+                    deque.addLast(pop.right);
+                }
+            }
+            res.add(cur);
+        }
+        return res;
+
+    }
+}
