7d452abe35ae9c6b2fe13fc3a13ed6e7df42716b
songwenjinger <3189064417@qq.com>
2022-10-08 17:26:53 +0800

    no106 从中序与后序遍历序列构造二叉树

--- src/com/demo/no01/No106.java
+++ src/com/demo/no01/No106.java
@@ -6,6 +6,9 @@ import com.common.TreeNodeCommonMethod;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 106. 从中序与后序遍历序列构造二叉树
+ */
 public class No106 {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         if (postorder.length == 0) {
