73b3e4109a5c1f664f4a2c60baf52f5ceffa93f1
songwenjinger <3189064417@qq.com>
2022-10-08 12:08:00 +0800

    no222  完全二叉树的节点个数

---
+++ src/com/demo/no02/No222_4.java
@@ -0,0 +1,41 @@
+package com.demo.no02;
+
+import com.common.TreeNode;
+
+public class No222_4 {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = getDepth(root.left);
+        int rightDepth = getDepth(root.right);
+        if (leftDepth == rightDepth) {// 左子树是满二叉树
+            // 2^leftDepth其实是 （2^leftDepth - 1） + 1 ，左子树 + 根结点
+            return (1 << leftDepth) + countNodes(root.right);
+        } else {// 右子树是满二叉树
+            return (1 << rightDepth) + countNodes(root.left);
+        }
+    }
+
+    private int getDepth(TreeNode root) {
+        int depth = 0;
+        while (root != null) {
+            root = root.left;
+            depth++;
+        }
+        return depth;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode1=new TreeNode(1);
+        TreeNode treeNode2=new TreeNode(2);
+        TreeNode treeNode3=new TreeNode(3);
+        TreeNode treeNode4=new TreeNode(4);
+
+        treeNode1.left=treeNode2;
+        treeNode1.right=treeNode3;
+        treeNode2.left=treeNode4;
+        No222_4 no222_4=new No222_4();
+        System.out.println(no222_4.countNodes(treeNode1));
+    }
+}
