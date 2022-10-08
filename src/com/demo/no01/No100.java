e82b23559e8c0d51c07d98ce443b949334f25980
songwenjinger <3189064417@qq.com>
2022-10-08 15:01:38 +0800

    no100  相同的树

---
+++ src/com/demo/no01/No100.java
@@ -0,0 +1,21 @@
+package com.demo.no01;
+
+import com.common.TreeNode;
+
+/**
+ * 100. 相同的树
+ */
+public class No100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
