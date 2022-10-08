30ebaae6074f7c8f7a813c25e8f8f48df2081d2c
songwenjinger <3189064417@qq.com>
2022-10-08 10:01:16 +0800

    no101. 对称二叉树

---
+++ src/com/demo/no01/No101.java
@@ -0,0 +1,28 @@
+package com.demo.no01;
+
+import com.common.TreeNode;
+
+/**
+ * 101. 对称二叉树
+ */
+public class No101 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return compare(root.left, root.right);
+    }
+
+    public boolean compare(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return compare(left.left, right.right) && compare(left.right, right.left);
+    }
+}
