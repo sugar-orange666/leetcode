b3a35f57652ba746688f9372c6557a5d92954d9e
songwenjinger <3189064417@qq.com>
2022-10-08 16:23:53 +0800

    no112  路径总和

---
+++ src/com/demo/no01/No112.java
@@ -0,0 +1,19 @@
+package com.demo.no01;
+
+import com.common.TreeNode;
+
+/**
+ * #112. 路径总和
+ */
+public class No112 {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+        return hasPathSum(root.left, targetSum - root.val) ||
+                hasPathSum(root.right, targetSum - root.val);
+    }
+}
