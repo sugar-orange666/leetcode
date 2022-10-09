9aa14a0778ffa63a19040656fdfefe638300437c
songwenjinger <3189064417@qq.com>
2022-10-09 11:29:23 +0800

    no236. 二叉树的最近公共祖先

---
+++ src/com/demo/no02/No236.java
@@ -0,0 +1,32 @@
+package com.demo.no02;
+
+import com.common.TreeNode;
+
+/**
+ * 236. 二叉树的最近公共祖先
+ */
+public class No236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left == null && right != null) {
+            return right;
+        } else if (left != null && right == null) {
+            return left;
+        } else {
+            return null;
+        }
+    }
+
+
+}
