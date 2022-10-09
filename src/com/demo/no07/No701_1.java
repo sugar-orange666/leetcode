f201528602f6fd36c3c1a70458337a334dc48beb
songwenjinger <3189064417@qq.com>
2022-10-09 12:04:10 +0800

    no701. 二叉搜索树中的插入操作

---
+++ src/com/demo/no07/No701_1.java
@@ -0,0 +1,20 @@
+package com.demo.no07;
+
+import com.common.TreeNode;
+
+public class No701_1 {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        }
+        if (val > root.val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+
+        return root;
+
+    }
+}
