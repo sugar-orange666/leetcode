ef9c21ebcf816ff1f91491ab87a121763d2d5f67
songwenjinger <3189064417@qq.com>
2022-10-09 11:44:33 +0800

    no235. 二叉搜索树的最近公共祖先

---
+++ src/com/demo/no02/No235.java
@@ -0,0 +1,21 @@
+package com.demo.no02;
+
+import com.common.TreeNode;
+
+/**
+ * 235. 二叉搜索树的最近公共祖先
+ */
+public class No235 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+        return null;
+    }
+}
