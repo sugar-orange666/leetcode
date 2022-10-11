76bf2c129ea1f8cf65efec5da7eef3958cd46d33
songwenjinger <3189064417@qq.com>
2022-10-11 11:55:47 +0800

    no669. 修剪二叉搜索树

---
+++ src/com/demo/no06/No669_1.java
@@ -0,0 +1,20 @@
+package com.demo.no06;
+
+import com.common.TreeNode;
+
+public class No669_1 {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        }
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+}
