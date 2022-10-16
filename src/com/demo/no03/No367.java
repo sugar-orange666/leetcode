593ee7c8272aafcd2aac10fd6e8960470a8d6136
songwenjinger <3189064417@qq.com>
2022-10-16 12:38:43 +0800

    no367. 有效的完全平方数

---
+++ src/com/demo/no03/No367.java
@@ -0,0 +1,25 @@
+package com.demo.no03;
+
+public class No367 {
+    public boolean isPerfectSquare(int num) {
+        int left = 1, right = num;
+        int mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if ((long) mid * mid == num) {
+                return true;
+            } else if (mid < num / mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        No367 no367 = new No367();
+        System.out.println(no367.isPerfectSquare(5));
+    }
+
+}
