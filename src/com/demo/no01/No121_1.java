7ddd5a8981bcf0fec0548b9bd8a4d305aa9affd0
songwenjinger <3189064417@qq.com>
2022-11-09 08:51:19 +0800

    no121. 买卖股票的最佳时机

---
+++ src/com/demo/no01/No121_1.java
@@ -0,0 +1,29 @@
+package com.demo.no01;
+
+import java.util.Arrays;
+
+/**
+ * 122. 买卖股票的最佳时机 II
+ */
+public class No121_1 {
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        for (int i = 1; i < prices.length; i++) {
+            //当前持有的最大现金
+            dp[i][0] = Math.max(-prices[i], dp[i - 1][0]);
+
+            //当前不持有的最大现金
+            dp[i][1] = Math.max(dp[i - 1][1], prices[i] + dp[i - 1][0]);
+        }
+
+
+        return dp[dp.length - 1][1];
+    }
+
+    public static void main(String[] args) {
+        No121_1 no121_1 = new No121_1();
+        System.out.println(no121_1.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+}
