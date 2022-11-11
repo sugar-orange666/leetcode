565be425b0ed269b6620fb4c52ba54a1100b1f9b
songwenjinger <3189064417@qq.com>
2022-11-11 09:55:18 +0800

    no300 最长递增子序列

---
+++ src/com/demo/no03/No300.java
@@ -0,0 +1,31 @@
+package com.demo.no03;
+
+import java.util.Arrays;
+
+/**
+ * 300. 最长递增子序列
+ */
+public class No300 {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return 1;
+        }
+
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int result = Integer.MIN_VALUE;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+        return result;
+    }
+
+}
