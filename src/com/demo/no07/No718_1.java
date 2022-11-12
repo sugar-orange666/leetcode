4c84af1e8bcbb9b888ab5a148d27d404cf700738
songwenjinger <3189064417@qq.com>
2022-11-12 14:03:20 +0800

    no718. 最长重复子数组

---
+++ src/com/demo/no07/No718_1.java
@@ -0,0 +1,33 @@
+package com.demo.no07;
+
+import com.common.ArrayCommon;
+
+import java.util.Arrays;
+
+/**
+ * 718. 最长重复子数组
+ */
+public class No718_1 {
+    public int findLength(int[] nums1, int[] nums2) {
+        int res = 0;
+        int[] dp = new int[nums2.length + 1];
+        for (int i = 1; i <= nums1.length; i++) {
+            for (int j = nums2.length; j > 0; j--) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[j] = dp[j - 1] + 1;
+                } else {
+                    dp[j] = 0;
+                }
+                res = Math.max(res, dp[j]);
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        return res;
+    }
+
+    public static void main(String[] args) {
+        No718_1 no718 = new No718_1();
+        System.out.println(no718.findLength(new int[]{1, 2, 3, 2, 1}, new int[]{3, 2, 1, 4, 7}));
+        System.out.println(no718.findLength(new int[]{3, 2, 1, 4, 7}, new int[]{1, 2, 3, 2, 1}));
+    }
+}
