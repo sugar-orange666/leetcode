43735e7a1d863ca423c1421ef396705433046c35
songwenjinger <3189064417@qq.com>
2023-03-12 11:34:33 +0800

    21. 合并两个有序链表

--- src/com/common/ListNode.java
+++ src/com/common/ListNode.java
@@ -1,5 +1,6 @@
 package com.common;
 
+
 public class ListNode {
 
     public int val;
@@ -9,5 +10,43 @@ public class ListNode {
         this.val = val;
     }
 
+    public static void addAtTail(ListNode head, int val) {
+        if (head == null) {
+            head = new ListNode(val);
+        }
+        ListNode p = head;
 
+        for (; p.next != null; p = p.next) {
+        }
+        p.next = new ListNode(val);
+    }
+
+    public static void print(ListNode head) {
+        ListNode p = head;
+        while (p != null) {
+            System.out.printf(p.val + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+
+    /**
+     * 通过数组创建链表，返回链表头节点，链表不包含虚拟头节点
+     *
+     * @param nums nums
+     * @return ListNode
+     */
+    public static ListNode createListByArray(int[] nums) {
+        if (nums == null) {
+            return null;
+        }
+        ListNode head = new ListNode(nums[0]);
+        ListNode pre = head;
+        for (int j = 1; j < nums.length; j++) {
+            pre.next = new ListNode(nums[j]);
+            pre = pre.next;
+        }
+        return head;
+    }
 }
---
+++ src/com/demo/no00/No21.java
@@ -0,0 +1,65 @@
+package com.demo.no00;
+
+import com.common.ListNode;
+
+import java.util.Arrays;
+
+/**
+ * 21. 合并两个有序链表
+ */
+public class No21 {
+
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode dummyNode = new ListNode(-1);
+        ListNode tail = dummyNode;
+        ListNode cur1 = list1;
+        ListNode cur2 = list2;
+        while (cur1 != null && cur2 != null) {
+            if (cur1.val <= cur2.val) {
+                ListNode next = cur1.next;
+                tail.next = cur1;
+                cur1.next = null;
+                tail = cur1;
+
+                cur1 = next;
+
+            } else {
+                ListNode next = cur2.next;
+                tail.next = cur2;
+                cur2.next = null;
+                tail = cur2;
+                cur2 = next;
+
+            }
+        }
+
+        if (cur1 != null) {
+            tail.next = cur1;
+        }
+        if (cur2 != null) {
+            tail.next = cur2;
+        }
+
+
+        return dummyNode.next;
+    }
+
+
+    public static void main(String[] args) {
+        No21 no21 = new No21();
+        ListNode node1 = ListNode.createListByArray(new int[]{1, 2, 4});
+        ListNode node2 = ListNode.createListByArray(new int[]{1, 3, 4});
+
+
+        ListNode node = no21.mergeTwoLists(node1, node2);
+        ListNode.print(node);
+    }
+}
+
