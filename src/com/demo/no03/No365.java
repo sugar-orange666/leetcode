3e4fdc93fc1fe3417f7feb3ccce08232e0b2aa5d
songwenjinger <13201656250@163.com>
2024-01-28 15:23:38 +0800

    365 水壶问题

---
+++ src/com/demo/no03/No365.java
@@ -0,0 +1,40 @@
+package com.demo.no03;
+
+/**
+ * 365 水壶问题
+ * @Date：2024/1/28 14:37
+ * @Description
+ */
+public class No365 {
+
+    public boolean canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
+        if (jug1Capacity + jug2Capacity < targetCapacity) {
+            return false;
+        }
+        if (jug1Capacity == 0 || jug2Capacity == 0) {
+            return targetCapacity == 0 || jug1Capacity + jug2Capacity == targetCapacity;
+        }
+        return targetCapacity % gcd(jug1Capacity, jug2Capacity) == 0;
+    }
+
+    //最大公约数
+    public int gcd(int x, int y) {
+        int remainder = x % y;
+        while (remainder != 0) {
+            x = y;
+            y = remainder;
+            remainder = x % y;
+        }
+        return y;
+    }
+
+
+    public static int gcd1(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
